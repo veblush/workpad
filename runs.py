20a099218090383034e2246087391677cab7186e
Esun Kim <veblush@google.com>
2020-04-10 16:18:22 -0700

    Add cpu-stat

--- runs.py
+++ runs.py
@@ -1,16 +1,44 @@
 #!/usr/bin/env python3
 
 import argparse
+import datetime
+import json
+import os
 import subprocess
 import sys
-import datetime
 import time
-import os
+from dataclasses import dataclass
 
 PERCENTILES = [0.01, 0.25, 0.50, 0.75, 0.99]
 
 
-def print_stats(start_time, end_time, results):
+@dataclass
+class CpuStat:
+  time: datetime.datetime
+  user: float = 0
+  nice: float = 0
+  system: float = 0
+  iowait: float = 0
+  steal: float = 0
+  idle: float = 0
+
+
+CPU_STAT_FIELDS = ['user', 'nice', 'system', 'iowait', 'steal', 'idle']
+
+
+def get_cpu_stat():
+  try:
+    out = subprocess.check_output(["iostat", "-c", "-o", "JSON"])
+    j = json.loads(out)
+    cpu_stat = j["sysstat"]["hosts"][0]["statistics"][0]["avg-cpu"]
+    return CpuStat(datetime.datetime.now(), cpu_stat['user'], cpu_stat['nice'],
+                   cpu_stat['system'], cpu_stat['iowait'], cpu_stat['steal'],
+                   cpu_stat['idle'])
+  except OSError:
+    return None
+
+
+def print_stats(start_time, end_time, results, cpu_stats):
   elapsed_time = datetime.datetime.now() - start_time
   print("Elapsed time: {0} sec".format(elapsed_time.total_seconds()))
   times = sorted(r[2] - r[1] for r in results)
@@ -18,9 +46,14 @@ def print_stats(start_time, end_time, results):
   for p in PERCENTILES:
     p_value = times[int(len(times) * p)].total_seconds()
     print("  p{0:.2f}: {1:.2f} sec".format(p, p_value))
+  if cpu_stats:
+    print("CPU:")
+    for f in CPU_STAT_FIELDS:
+      values = [getattr(stat, f) for stat in cpu_stats]
+      print("  {0}: {1}".format(f, values[len(values) // 2]))
 
 
-def write_report(start_time, end_time, results, args):
+def write_report(start_time, end_time, results, cpu_stats, args):
   elapsed_time = datetime.datetime.now() - start_time
   times = sorted(r[2] - r[1] for r in results)
   is_new_file = not os.path.exists(args.report_file)
@@ -29,20 +62,29 @@ def write_report(start_time, end_time, results, args):
       f.write("\t".join([
           "Time", "Tag", "Elapsed", "Threads", "Runs", "Run-P01-T", "Run-P25-T",
           "Run-P50-T", "Run-P75-T", "Run-P99-T"
-      ]) + "\n")
+      ] + ["CPU-" + f for f in CPU_STAT_FIELDS]) + "\n")
     f.write("{0}\t{1}\t{2:.2f}\t{3}\t{4}".format(start_time, args.report_tag,
                                                  elapsed_time.total_seconds(),
                                                  args.threads, args.runs))
     for p in PERCENTILES:
       p_value = times[int(len(times) * p)].total_seconds()
       f.write("\t{0:.2f}".format(p_value))
+    for field in CPU_STAT_FIELDS:
+      if cpu_stats:
+        values = [getattr(stat, field) for stat in cpu_stats]
+        v = values[len(values) // 2]
+      else:
+        v = 0
+      f.write("\t{0}".format(v))
     f.write("\n")
 
 
 def run(args, cmds):
   processes = []
   results = []
+  cpu_stats = []
   start_time = datetime.datetime.now()
+  last_stat_time = None
   while len(results) < args.runs:
     for i in range(len(processes) - 1, -1, -1):
       p = processes[i][0]
@@ -59,12 +101,19 @@ def run(args, cmds):
         print("Run")
       p = subprocess.Popen(cmds)
       processes.append((p, datetime.datetime.now()))
+    now = datetime.datetime.now()
+    if last_stat_time is None or (now - last_stat_time).total_seconds() >= 1:
+      last_stat_time = now
+      stat = get_cpu_stat()
+      if stat:
+        cpu_stats.append(stat)
     time.sleep(0.01)
+
   end_time = datetime.datetime.now()
   if args.stats:
-    print_stats(start_time, end_time, results)
+    print_stats(start_time, end_time, results, cpu_stats)
   if args.report_file:
-    write_report(start_time, end_time, results, args)
+    write_report(start_time, end_time, results, cpu_stats, args)
 
 
 def main():
