803f4ffbe8e750c31e2657a8820aadd3c0d99421
Esun Kim <veblush@google.com>
2020-04-10 23:08:07 -0700

    Fix

--- benchmark/job_java_gcs_gradle.sh
+++ benchmark/job_java_gcs_gradle.sh
@@ -10,6 +10,6 @@ GCS_PATH=$HOME/grpc-gcp-java/end2end-test-examples/gcs
 
 pushd $GCS_PATH
 
-/gradlew run --args="--client=$1 --bkt=$2 --obj=$3 --buffSize=1048576 --method=read --dp=true --calls=$4"
+./gradlew run --args="--client=$1 --bkt=$2 --obj=$3 --buffSize=1048576 --method=read --dp=true --calls=$4"
 
 popd
