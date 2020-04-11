65b612b8cffe2cff5c7b23025fb17b19dc44439b
Esun Kim <veblush@google.com>
2020-04-10 18:35:25 -0700

    Update script

--- benchmark/run_curl.sh
+++ benchmark/run_curl.sh
@@ -5,7 +5,7 @@ for i in {1..100}
     for t in 1 2 4 6 8 10 12 14 16 18 20
       do
         echo ----- iteration:$i threads:$t -----
-        ../runs.py -r 100 -t $t \
+        ../runs.py -r $(($t*20)) -t $t \
           --report_tag $t \
           --report_file=$REPORT_FILE \
           -- \
--- benchmark/run_gcs_java.sh
+++ benchmark/run_gcs_java.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+if [ -z "$1" ] ; then
+    echo "Client is needed. yoshi, grpc, gcsio-http, and gcsio-grpc"
+    exit
+fi
 JAVA=/usr/lib/jvm/adoptopenjdk-8-hotspot-amd64/bin/java
 TEST_JAR=~/grpc-gcp-java/end2end-test-examples/gcs/build/libs/gcs-all-1.0-SNAPSHOT.jar
-REPORT_FILE=~/log/report-yoshi.tsv
+REPORT_FILE=~/log/report-gcs-java-$1.tsv
 for i in {1..100}
   do
     for t in 1 2 4 6 8 10 12 14 16 18 20
@@ -13,11 +17,12 @@ for i in {1..100}
           -- \
           $JAVA \
           -jar $TEST_JAR \
-          --client=yoshi \
+          --client=$1 \
           --bkt=gcs-grpc-team-veblush1 \
           --obj=1GB.bin \
           --buffSize=1048576 \
           --method=read \
+          --dp=true \
           --calls=20
       done
   done
