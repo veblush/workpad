4b30114e3fca05d54a5e2db090dd4bdefc324fe0
Esun Kim <veblush@google.com>
2020-04-10 17:59:28 -0700

    Comment

--- benchmark/run_curl.sh
+++ benchmark/run_curl.sh
@@ -4,7 +4,7 @@ for i in {1..100}
   do
     for t in 1 2 4 6 8 10 12 14 16 18 20
       do
-        echo ***** iteration:$i threads:$t
+        echo ----- iteration:$i threads:$t -----
         ../runs.py -r 100 -t $t \
           --report_tag $t \
           --report_file=$REPORT_FILE \
