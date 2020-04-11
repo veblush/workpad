99f1af4d65efae060fb6219b6bbed35139f30f76
Esun Kim <veblush@google.com>
2020-04-11 14:43:34 -0700

    Add more stats

--- runs.py
+++ runs.py
@@ -26,11 +26,36 @@ class CpuStat:
 CPU_STAT_FIELDS = ['user', 'nice', 'system', 'iowait', 'steal', 'idle']
 
 
+@dataclass
+class MemStat:
+  time: datetime.datetime
+  total: int = 0
+  used: int = 0
+  free: int = 0
+  shared: int = 0
+  cache: int = 0
+  available: int = 0
+
+
+MEM_STAT_FIELDS = ['total', 'used', 'free', 'shared', 'cache', 'available']
+
+
+@dataclass
+class NetStat:
+  time: datetime.datetime
+  in_bandwith: int = 0
+  out_bandwith: int = 0
+
+
+NET_STAT_FIELDS = ['in_bandwith', 'out_bandwith']
+
+
 def init_cpu_stat_proc():
   try:
     return subprocess.Popen(["iostat", "-c", "-o", "JSON", "1", "2"],
                             stdout=subprocess.PIPE)
-  except OSError:
+  except OSError as e:
+    print(e)
     return None
 
 
@@ -44,40 +69,101 @@ def get_cpu_stat(p):
                  cpu_stat['idle'])
 
 
-def print_stats(start_time, end_time, results, cpu_stats):
+def get_mem_stat():
+  try:
+    o = subprocess.check_output(["free"])
+    for l in o.decode('utf-8').split("\n"):
+      if l.startswith("Mem:"):
+        values = [int(w) for w in l[5:].split()]
+        if len(values) == 6:
+          return MemStat(datetime.datetime.now(), values[0], values[1],
+                         values[2], values[3], values[4], values[5])
+    return None
+  except OSError as e:
+    print(e)
+    return None
+
+
+def init_net_stat_proc():
+  try:
+    return subprocess.Popen(["ifstat", "-Tq", "1", "1"], stdout=subprocess.PIPE)
+  except OSError as e:
+    print(e)
+    return None
+
+
+def get_net_stat(p):
+  if p is None:
+    return None
+  o = p.communicate()[0]
+  l = [l for l in o.decode('utf-8').split("\n") if l.strip()][-1]
+  values = [int(float(w) * 1024) for w in l.split()]
+  if len(values) >= 2:
+    return NetStat(datetime.datetime.now(), values[-2], values[-1])
+  return None
+
+
+def print_stats(start_time, end_time, results, sys_stats):
   elapsed_time = datetime.datetime.now() - start_time
+  error_runs = sum(1 if r[0] != 0 else 0 for r in results)
   print("Elapsed time: {0} sec".format(elapsed_time.total_seconds()))
+  print("Total runs: {0} errors: {1}".format(len(results), error_runs))
   times = sorted(r[2] - r[1] for r in results)
   print("Run:")
   for p in PERCENTILES:
     p_value = times[int(len(times) * p)].total_seconds()
     print("  p{0:.2f}: {1:.2f} sec".format(p, p_value))
-  if cpu_stats:
+  if sys_stats:
     print("CPU:")
     for f in CPU_STAT_FIELDS:
-      values = [getattr(stat, f) for stat in cpu_stats]
+      values = [getattr(sys_stat[0], f) for sys_stat in sys_stats]
+      print("  {0}: {1:.2f}".format(f, sum(values) / len(values)))
+    print("MEM:")
+    for f in MEM_STAT_FIELDS:
+      values = [getattr(sys_stat[1], f) for sys_stat in sys_stats]
+      print("  {0}: {1:.2f}".format(f, sum(values) / len(values)))
+    print("NET:")
+    for f in NET_STAT_FIELDS:
+      values = [getattr(sys_stat[2], f) for sys_stat in sys_stats]
       print("  {0}: {1:.2f}".format(f, sum(values) / len(values)))
 
 
-def write_report(start_time, end_time, results, cpu_stats, args):
+def write_report(start_time, end_time, results, sys_stats, args):
   elapsed_time = datetime.datetime.now() - start_time
+  error_runs = sum(1 if r[0] != 0 else 0 for r in results)
   times = sorted(r[2] - r[1] for r in results)
   is_new_file = not os.path.exists(args.report_file)
   with open(args.report_file, "at") as f:
     if is_new_file:
       f.write("\t".join([
-          "Time", "Tag", "Elapsed", "Threads", "Runs", "Run-P01-T", "Run-P25-T",
+          "Time", "Tag", "Elapsed", "Threads", "Runs", "Errors", "Run-P01-T", "Run-P25-T",
           "Run-P50-T", "Run-P75-T", "Run-P99-T"
-      ] + ["CPU-" + f for f in CPU_STAT_FIELDS]) + "\n")
+      ] + ["CPU-" + f
+           for f in CPU_STAT_FIELDS] + ["MEM-" + f for f in MEM_STAT_FIELDS] +
+                        ["NET-" + f for f in NET_STAT_FIELDS]) + "\n")
     f.write("{0}\t{1}\t{2:.2f}\t{3}\t{4}".format(start_time, args.report_tag,
                                                  elapsed_time.total_seconds(),
-                                                 args.threads, args.runs))
+                                                 args.threads, args.runs, error_runs))
     for p in PERCENTILES:
       p_value = times[int(len(times) * p)].total_seconds()
       f.write("\t{0:.2f}".format(p_value))
     for field in CPU_STAT_FIELDS:
-      if cpu_stats:
-        values = [getattr(stat, field) for stat in cpu_stats]
+      if sys_stats:
+        values = [getattr(sys_stat[0], field) for sys_stat in sys_stats]
+        v = sum(values) / len(values)
+      else:
+        v = 0
+      f.write("\t{0:.2f}".format(v))
+    for field in MEM_STAT_FIELDS:
+      if sys_stats:
+        values = [getattr(sys_stat[1], field) for sys_stat in sys_stats]
+        v = sum(values) / len(values)
+      else:
+        v = 0
+      f.write("\t{0:.2f}".format(v))
+    for field in NET_STAT_FIELDS:
+      if sys_stats:
+        values = [getattr(sys_stat[2], field) for sys_stat in sys_stats]
         v = sum(values) / len(values)
       else:
         v = 0
@@ -88,10 +174,11 @@ def write_report(start_time, end_time, results, cpu_stats, args):
 def run(args, cmds):
   processes = []
   results = []
-  cpu_stats = []
+  sys_stats = []
   cpu_stats_proc = init_cpu_stat_proc()
+  net_stats_proc = init_net_stat_proc()
   start_time = datetime.datetime.now()
-  last_stat_time = None
+  # Dispatching processes running cmd
   while len(results) < args.runs:
     for i in range(len(processes) - 1, -1, -1):
       p = processes[i][0]
@@ -108,18 +195,22 @@ def run(args, cmds):
         print("Run")
       p = subprocess.Popen(cmds)
       processes.append((p, datetime.datetime.now()))
-    if cpu_stats_proc.poll() != None:
-      stat = get_cpu_stat(cpu_stats_proc)
-      if stat:
-        cpu_stats.append(stat)
+    # Collect system stats
+    if cpu_stats_proc.poll() != None and net_stats_proc.poll() != None:
+      cpu_stat = get_cpu_stat(cpu_stats_proc)
+      mem_stat = get_mem_stat()
+      net_stat = get_net_stat(net_stats_proc)
+      if cpu_stat and mem_stat and net_stat:
+        sys_stats.append((cpu_stat, mem_stat, net_stat))
       cpu_stats_proc = init_cpu_stat_proc()
+      net_stats_proc = init_net_stat_proc()
     time.sleep(0.01)
-
+  # Reports when done
   end_time = datetime.datetime.now()
   if args.stats:
-    print_stats(start_time, end_time, results, cpu_stats)
+    print_stats(start_time, end_time, results, sys_stats)
   if args.report_file:
-    write_report(start_time, end_time, results, cpu_stats, args)
+    write_report(start_time, end_time, results, sys_stats, args)
 
 
 def main():
