2e1d547c1302bda4272fb299c96188c5509ad280
Esun Kim <veblush@google.com>
2020-04-21 17:50:34 -0700

    Add perf

--- runs.py
+++ runs.py
@@ -3,13 +3,38 @@
 import argparse
 import datetime
 import json
+import locale
 import os
+import re
 import subprocess
 import sys
+import tempfile
 import time
 from dataclasses import dataclass
 
 PERCENTILES = [0.01, 0.25, 0.50, 0.75, 0.99]
+
+
+@dataclass
+class PerfStat:
+  task_clock: float = 0
+  context_switches: int = 0
+  cpu_migrations: int = 0
+  page_faults: int = 0
+  cycles: int = 0
+  instructions: int = 0
+  branches: int = 0
+  branch_misses: int = 0
+  wall_time: float = 0
+  user_time: float = 0
+  sys_time: float = 0
+
+
+PERF_STAT_FIELDS = [
+    'task_clock', 'context_switches', 'cpu_migrations', 'page_faults', 'cycles',
+    'instructions', 'branches', 'branch_misses', 'wall_time', 'user_time',
+    'sys_time'
+]
 
 
 @dataclass
@@ -103,6 +128,45 @@ def get_net_stat(p):
   return None
 
 
+def parse_perf_stat_output(output):
+  stat = PerfStat()
+  mo = re.search(r"([0-9.,]+)\s+msec\s+task-clock", output)
+  if mo:
+    stat.task_clock = locale.atof(mo.group(1))
+  mo = re.search(r"([0-9.,]+)\s+context-switches", output)
+  if mo:
+    stat.context_switches = locale.atoi(mo.group(1))
+  mo = re.search(r"([0-9.,]+)\s+cpu-migrations", output)
+  if mo:
+    locale.atoi
+    stat.cpu_migrations = locale.atoi(mo.group(1))
+  mo = re.search(r"([0-9.,]+)\s+page-faults", output)
+  if mo:
+    stat.page_faults = locale.atoi(mo.group(1))
+  mo = re.search(r"([0-9.,]+)\s+cycles", output)
+  if mo:
+    stat.cycles = locale.atoi(mo.group(1))
+  mo = re.search(r"([0-9.,]+)\s+instructions", output)
+  if mo:
+    stat.instructions = locale.atoi(mo.group(1))
+  mo = re.search(r"([0-9.,]+)\s+branches", output)
+  if mo:
+    stat.branches = locale.atoi(mo.group(1))
+  mo = re.search(r"([0-9.,]+)\s+branch-misses", output)
+  if mo:
+    stat.branch_misses = locale.atoi(mo.group(1))
+  mo = re.search(r"([0-9.,]+)\s+seconds time elapsed", output)
+  if mo:
+    stat.wall_time = locale.atof(mo.group(1))
+  mo = re.search(r"([0-9.,]+)\s+seconds user", output)
+  if mo:
+    stat.user_time = locale.atof(mo.group(1))
+  mo = re.search(r"([0-9.,]+)\s+seconds sys", output)
+  if mo:
+    stat.sys_time = locale.atof(mo.group(1))
+  return stat
+
+
 def print_stats(start_time, end_time, results, sys_stats):
   elapsed_time = datetime.datetime.now() - start_time
   error_runs = sum(1 if r[0] != 0 else 0 for r in results)
@@ -113,6 +177,11 @@ def print_stats(start_time, end_time, results, sys_stats):
   for p in PERCENTILES:
     p_value = times[int(len(times) * p)].total_seconds()
     print("  p{0:.2f}: {1:.2f} sec".format(p, p_value))
+  perf_stats = list(sorted((r[3] for r in results), key=lambda x: x.task_clock))
+  perf_stat_mid = perf_stats[len(perf_stats) // 2]
+  print("Perf (p50):")
+  for f in PERF_STAT_FIELDS:
+    print("  {0}: {1}".format(f, getattr(perf_stat_mid, f)))
   if sys_stats:
     print("CPU:")
     for f in CPU_STAT_FIELDS:
@@ -135,18 +204,26 @@ def write_report(start_time, end_time, results, sys_stats, args):
   is_new_file = not os.path.exists(args.report_file)
   with open(args.report_file, "at") as f:
     if is_new_file:
-      f.write("\t".join([
+      all_columns = ([
           "Time", "Tag", "Elapsed", "Threads", "Runs", "Errors", "Run-P01-T",
           "Run-P25-T", "Run-P50-T", "Run-P75-T", "Run-P99-T"
-      ] + ["CPU-" + f
-           for f in CPU_STAT_FIELDS] + ["MEM-" + f for f in MEM_STAT_FIELDS] +
-                        ["NET-" + f for f in NET_STAT_FIELDS]) + "\n")
+      ] + ["PERF-" + f for f in PERF_STAT_FIELDS] +
+                     ["CPU-" + f for f in CPU_STAT_FIELDS] +
+                     ["MEM-" + f for f in MEM_STAT_FIELDS] +
+                     ["NET-" + f for f in NET_STAT_FIELDS])
+      f.write("\t".join(all_columns) + "\n")
     f.write("{0}\t{1}\t{2:.2f}\t{3}\t{4}\t{5}".format(
         start_time, args.report_tag, elapsed_time.total_seconds(), args.threads,
         args.runs, error_runs))
     for p in PERCENTILES:
       p_value = times[int(len(times) * p)].total_seconds()
       f.write("\t{0:.2f}".format(p_value))
+    perf_stats = list(
+        sorted((r[3] for r in results), key=lambda x: x.task_clock))
+    perf_stat_mid = perf_stats[len(perf_stats) // 2]
+    for field in PERF_STAT_FIELDS:
+      v = getattr(perf_stat_mid, field)
+      f.write("\t{0:.2f}".format(v))
     for field in CPU_STAT_FIELDS:
       if sys_stats:
         values = [getattr(sys_stat[0], field) for sys_stat in sys_stats]
@@ -184,17 +261,23 @@ def run(args, cmds):
       p = processes[i][0]
       if p.poll() != None:
         now = datetime.datetime.now()
+        start_time = processes[i][1]
+        perf_stat_file = processes[i][2]
+        with open(perf_stat_file, 'r') as f:
+          perf_stat = parse_perf_stat_output(f.read())
+        if perf_stat:
+          os.remove(perf_stat_file)
         if args.verbose:
-          print("Done: Code={0} Elapsed={1}".format(p.poll(),
-                                                    now - processes[i][1]))
-        results.append((p.poll(), processes[i][1], now))
+          print("Done: Code={0} Elapsed={1}".format(p.poll(), now - start_time))
+        results.append((p.poll(), start_time, now, perf_stat))
         del processes[i]
     while len(
         processes) < args.threads and len(processes) + len(results) < args.runs:
       if args.verbose:
         print("Run")
-      p = subprocess.Popen(cmds)
-      processes.append((p, datetime.datetime.now()))
+      perf_stat_file = tempfile.mkstemp(prefix="runs_perf_")[1]
+      p = subprocess.Popen(["perf", "stat", "-o", perf_stat_file] + cmds)
+      processes.append((p, datetime.datetime.now(), perf_stat_file))
     # Collect system stats
     if cpu_stats_proc.poll() != None and net_stats_proc.poll() != None:
       cpu_stat = get_cpu_stat(cpu_stats_proc)
@@ -247,6 +330,7 @@ def main():
   argv = sys.argv[1:idx]
   cmds = sys.argv[idx + 1:]
 
+  locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
   args = parser.parse_args(argv)
   run(args, cmds)
 
