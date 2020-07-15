f3d7481eea6dd74df924008d4e97c59bc539c71f
Esun Kim <veblush@google.com>
2020-07-14 17:05:00 -0700

    Fix

--- benchmark/job_curl_write.sh
+++ benchmark/job_curl_write.sh
@@ -11,8 +11,8 @@ ACCESS_TOKEN=`gcloud auth print-access-token`
 URL="https://www.googleapis.com/upload/storage/v1/b/$1/o?uploadType=media&name=${2}_${RUN_PROCESS_ID}"
 for i in $(seq 2 $4); do
   if [ $UPLOAD_FILE_DIRECTLY ]; then
-    url -X POST --data-binary "@$3" -H "Authorization: Bearer $ACCESS_TOKEN" $URL > /dev/null
+    curl -X POST --data-binary "@$3" -H "Authorization: Bearer $ACCESS_TOKEN" $URL > /dev/null
   else
-    cat "$3" | url -X POST --data-binary @- -H "Authorization: Bearer $ACCESS_TOKEN" $URL > /dev/null
+    cat "$3" | curl -X POST --data-binary @- -H "Authorization: Bearer $ACCESS_TOKEN" $URL > /dev/null
   fi
 done
